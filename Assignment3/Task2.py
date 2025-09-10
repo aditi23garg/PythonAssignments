e7115b64ec94ffa430bb0e20ea916aaf01c3a9fe
aditi23garg <aditi23garg@gmail.com>
2025-09-10 12:32:50 +0530

    Commit

---
+++ Assignment3/Task2.py
@@ -0,0 +1,8 @@
+import math
+num = int(input("Enter a number: "))
+square = math.sqrt(num)
+print("Square root:", square)
+logarithm = math.log(num)
+print("Logarithm:", logarithm)
+sine = math.sin(num)
+print("Sine:", sine)
